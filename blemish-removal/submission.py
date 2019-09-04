112a9181d49a246a45bf8567c059d793ada74719
Paweł Grudzień <p.grudzien12@live.com>
2019-09-04 23:41:18 +0200

    yeah

--- blemish-removal/submission.py
+++ blemish-removal/submission.py
@@ -4,17 +4,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
 
+def calcVariance(patch):
+        x = cv2.Scharr(patch, -1, 1, 0)
+        y = cv2.Scharr(patch, -1, 0, 1)
+
+        return np.abs(x) + np.abs(y)
+
 def onMouse(action, x, y, flags, userdata): 
     if action==cv2.EVENT_LBUTTONDOWN:
-        center=[(x,y)]
         # Mark the center
         patch = userdata[y-15:y+15,x-15:x+15]
-        patch = cv2.cvtColor(patch, cv2.COLOR_BGR2GRAY)
-        patch = cv2.Laplacian(patch, -1)
-        resized = cv2.resize(patch, (200,200))
-        cv2.setWindowTitle("patch", "Second derivative = {0}".format(np.sum(patch)))
-        cv2.imshow("patch", resized)
-
+        patch = cv2.cvtColor(patch, cv2.COLOR_BGR2HSV)[:,:,0]
+        variance = calcVariance(patch)
+        size = (400,400)
+        resizedVariance = cv2.resize(variance, size)
+        resizedPatch = cv2.resize(patch, size)
+        cv2.setWindowTitle("patch", "{0}".format(np.sum(variance)))
+        cv2.imshow("patch", np.hstack([resizedVariance, resizedPatch]))
 
 source = cv2.imread("blemish-removal/blemish.png",1)
 # Make a dummy image, will be useful to clear the drawing
