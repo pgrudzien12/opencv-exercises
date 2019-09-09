3a2e78d72483a5f55eb5428d6b3c6abacbda9568
Paweł Grudzień <pawel.grudzien@avanade.com>
2019-09-09 17:33:58 +0200

    experiments

--- chroma-keying/submission.py
+++ chroma-keying/submission.py
@@ -1,14 +1,18 @@
 import cv2
 import numpy as np
+from datetime import datetime
 
 toleranceFactor = 10
 softnessFactor = 10
-defringeFactor = 10
+defringeFactor = 100
+green = 60
 
 windowName = "Video"
+frame = None
 
 # load an image
-cap = cv2.VideoCapture("greenscreen-asteroid.mp4")
+#cap = cv2.VideoCapture("greenscreen-asteroid.mp4")
+cap = cv2.VideoCapture("greenscreen-demo.mp4")
 if (cap.isOpened()== False): 
     print("Error opening video stream or file")
 property_id = int(cv2.CAP_PROP_FRAME_COUNT) 
@@ -17,15 +21,46 @@ capLength = int(cv2.VideoCapture.get(cap, property_id))
 # Create a window to display results
 cv2.namedWindow(windowName, cv2.WINDOW_AUTOSIZE)
 
+def onMouse(action, x, y, flags, userdata):
+    global green, frame
+    if action==cv2.EVENT_LBUTTONDOWN:
+        image_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        green = np.median(image_hsv[y-3:y+3,x-3:x+3,0])
+    if action==cv2.EVENT_RBUTTONDOWN:
+        patch = frame[y-40:y+40,x-40:x+40]
+        cv2.imshow("patch", cv2.resize(patch, (400,400)))
+    return
+
 def buildToleranceMask(img_hsv, toleranceFactor, green):
     hue = img_hsv[:,:,0]
-    tolerance = (toleranceFactor / 100)* green
+    saturation = img_hsv[:,:,1]
+    value = img_hsv[:,:,2]
+    tolerance = (toleranceFactor / 100) * green
     lb_green, hb_green = green - tolerance, green + tolerance
     hue_tolerance = np.logical_and(lb_green <= hue, hue <= hb_green)
+    saturated = np.logical_and(hue_tolerance, saturation >= 60)
+    valued = np.logical_and(saturated, value >= 10)
     value_mask = np.ones_like(hue) * 255
-    value_mask[hue_tolerance] = 0
+    value_mask[valued] = 0
     return value_mask
 
+def defringe(image, mask, defringeFactor):
+    # pivot points for X-Coordinates
+    originalValue = np.array([0, 50, 100, 150, 200, 255])
+    factor = (defringeFactor) / 100
+    # Changed points on Y-axis for each channel
+    gCurve = originalValue * factor
+
+    # Create a LookUp Table
+    fullRange = np.arange(0,256)
+    gLUT = np.interp(fullRange, originalValue, gCurve)
+
+    # Get the red channel and apply the mapping
+    gChannel = image[:,:,1]
+    gChannel = cv2.LUT(gChannel, gLUT)
+    image[:,:,1] = gChannel
+
+    return image
 
 def blurMask(mask, softnessFactor):
     ksize = softnessFactor // 2
@@ -34,20 +69,42 @@ def blurMask(mask, softnessFactor):
 
     size = (ksize, ksize)
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, size)
-    morphed = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    morphed = cv2.erode(morphed, kernel)
-    return cv2.GaussianBlur(morphed, size, 2)
+    morphed = mask.copy()
+    #morphed = cv2.morphologyEx(morphed, cv2.MORPH_CLOSE, kernel)
+    #morphed = cv2.erode(morphed, kernel)
+    #morphed = cv2.GaussianBlur(morphed, size, 2)
+    return morphed
+
+
+def getMaskEdges(mask):
+    lowThreshold = 100
+    ratio = 3
+    kernelSize = 3
+    size = (31, 31)
+    edges = cv2.Canny(mask, lowThreshold, lowThreshold * ratio, kernelSize);
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, size)
+    edges = cv2.dilate(edges, kernel, iterations=2)
+    cv2.imshow("edges", edges)
+
+    return None
+
+def preprocess(image):
+    return cv2.GaussianBlur(image, (3,3), 2)
 
 def process(image_original):
-    global toleranceFactor, softnessFactor, defringeFactor
-
-    image_hsv = cv2.cvtColor(image_original, cv2.COLOR_BGR2HSV)
-    mask = buildToleranceMask(image_hsv, toleranceFactor, 60)
-    mask = blurMask(mask, softnessFactor)
+    global toleranceFactor, softnessFactor, defringeFactor, green
+    image = image_original
+#    image = preprocess(image_original)
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = buildToleranceMask(image_hsv, toleranceFactor, green)
+    mask_edges = getMaskEdges(mask)
+    #mask = blurMask(mask, softnessFactor)
 
     image_hsv[:,:,2] = np.bitwise_and(image_hsv[:,:,2], mask)
 
-    return cv2.cvtColor(image_hsv, cv2.COLOR_HSV2BGR)
+    color_image = cv2.cvtColor(image_hsv, cv2.COLOR_HSV2BGR)
+    
+    return defringe(color_image, mask, defringeFactor)
 
 def onChangeTolerance():
     global toleranceFactor
@@ -70,6 +127,7 @@ def onPositionChange(*args):
 
 play = True
 readOne = False
+cv2.setMouseCallback(windowName, onMouse)
 
 while(cap.isOpened()):
     # Capture frame-by-frame
@@ -78,8 +136,7 @@ while(cap.isOpened()):
         if ret == True:
             resultFrame = frame
         readOne = False
-
-    key = cv2.waitKey(25)
+    key = cv2.waitKey(15)
     if key & 0xFF == 27:
         break
     
@@ -88,6 +145,7 @@ while(cap.isOpened()):
 
     # Display the resulting frame
     cv2.imshow(windowName, process(resultFrame))
+    cv2.setWindowTitle(windowName, "Image Green Hue={0}".format(green))
 
     # display progress trackbar
     cFrameNo = cap.get(cv2.CAP_PROP_POS_FRAMES); 
